075ace66cf616814d59c29cc61ba41bd6abb9314
Qubra <qubra@zerostack.com>
2020-11-04 21:41:09 +0530

    Updates

---
+++ precache-manifest.7d1e8282d55856822fd763e6ce4037c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8febfa879f4630f2328f5520665f0e8",
+    "url": "/monsters-rolodex-complete/index.html"
+  },
+  {
+    "revision": "53fae0bb065f43d63640",
+    "url": "/monsters-rolodex-complete/static/css/main.a142fb8a.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex-complete/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex-complete/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53fae0bb065f43d63640",
+    "url": "/monsters-rolodex-complete/static/js/main.b2e64b2a.chunk.js"
+  },
+  {
+    "revision": "4b601965c2149392e1f2",
+    "url": "/monsters-rolodex-complete/static/js/runtime-main.b3c2bfd0.js"
+  }
+]);
